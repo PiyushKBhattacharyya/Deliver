629f2523df21e191fdfc327b632ba892938e06cd
Piyush <piyushbhattacharyya@gmail.com>
2023-02-24 02:23:06 +0530

    commit_02:23

--- deliver/deliver/urls.py
+++ deliver/deliver/urls.py
@@ -1,7 +1,7 @@
 """deliver URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from customer.views import Index, About
+from customer.views import Index, About, Order, OrderConfirmation, OrderPayConfirmation, Menu, MenuSearch
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('restaurant/', include('restaurant.urls')),
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
+    path('menu/', Menu.as_view(), name='menu'),
+    path('menu/search/', MenuSearch.as_view(), name='menu-search'),
+    path('order/', Order.as_view(), name='order'),
+    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(),
+         name='order-confirmation'),
+    path('payment-confirmation/', OrderPayConfirmation.as_view(),
+         name='payment-confirmation'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
