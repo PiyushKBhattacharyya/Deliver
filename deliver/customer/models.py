756d361cba6007b4f87c0360f3a99cd254ed0d54
Piyush <piyushbhattacharyya@gmail.com>
2023-02-24 01:22:03 +0530

    commit_01:22

--- deliver/customer/models.py
+++ deliver/customer/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-class Order(models.Model):
+class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
--- deliver/customer/views.py
+++ deliver/customer/views.py
@@ -8,3 +8,17 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
+    
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+        main_course = MenuItem.objects.filter(category__name__contains='Main Course')
+
+        context = {
+            'appetizers' : appetizers,
+            'main_course' : main_course,
+        }
+
+        return render(request, 'customer/order.html', context)
+    
+    def post(self, request, *args, **kwargs):
